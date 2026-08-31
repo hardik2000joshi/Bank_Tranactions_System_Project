9c4f06cea6222af0954dc5e9e55021db826d58bb
hardik2000joshi <hardik2000joshi@gmail.com>
2026-08-31 17:21:07 +0530

    Organize project into Backend and Frontend

--- server.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config()
-const app = require("./src/app");
-const connectToDB = require("./src/database/db");
-connectToDB()
-app.listen(3003, () => {
-    console.log("Server is running on port 3003");
-})
--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const auth = require("../controller/authController");
-const router = express.Router();
-
-/* POST /api/auth/register */
-router.post("/register", auth.registerUser);
-router.post("/login", auth.loginUser);
-/* POST /api/auth/login */
-module.exports = router;
